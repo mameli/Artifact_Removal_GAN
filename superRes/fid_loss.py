f837f936f24166c55aa2c317b107bb658ff598a4
mameli <mameli93@gmail.com>
2019-11-13 14:44:56 +0100

    fid loss

--- superRes/fid_loss.py
+++ superRes/fid_loss.py
@@ -42,11 +42,21 @@ class MatrixSquareRoot(Function):
     
 sqrtm = MatrixSquareRoot.apply
 
-def get_activations(imgs, model, dims=2048, cuda=True):
-    model.cuda()  
+def get_activations(imgs, model, dim=2048):
+    """Calculates the activations of the pool_3 layer for all images.
+
+    Params:
+    -- imgs        : Tensor of images
+    -- model       : Instance of inception model
+    -- dims        : Dimensionality of features returned by Inception
     
+    Returns:
+    -- A pytorch tensor of dimension (num images, dims) that contains the
+       activations of the given tensor when feeding inception with the
+       query tensor.
+    """
     n_imgs = len(imgs)
-    pred_arr = torch.empty((n_imgs, dims))
+    pred_arr = torch.empty((n_imgs, dim))
 
     imgs = imgs.cuda()
 
@@ -58,6 +68,26 @@ def get_activations(imgs, model, dims=2048, cuda=True):
 
 
 def calculate_frechet_distance(mu1, sigma1, mu2, sigma2):
+    """Numpy implementation of the Frechet Distance.
+    The Frechet distance between two multivariate Gaussians X_1 ~ N(mu_1, C_1)
+    and X_2 ~ N(mu_2, C_2) is
+            d^2 = ||mu_1 - mu_2||^2 + Tr(C_1 + C_2 - 2*sqrt(C_1*C_2)).
+
+    Stable version by Dougal J. Sutherland.
+
+    Params:
+    -- mu1   : Pytorch tensor containing the activations of a layer of the
+               inception net (like returned by the function 'get_predictions')
+               for generated samples.
+    -- mu2   : The sample mean over activations, precalculated on an
+               representative data set.
+    -- sigma1: The covariance matrix over activations for generated samples.
+    -- sigma2: The covariance matrix over activations, precalculated on an
+               representative data set.
+
+    Returns:
+    --   : The Frechet Distance.
+    """
     diff = mu1 - mu2
 
     covmean = sqrtm(torch.mm(sigma1, sigma2))
@@ -65,28 +95,40 @@ def calculate_frechet_distance(mu1, sigma1, mu2, sigma2):
     tr_covmean = torch.trace(covmean)
 
     fid = (torch.dot(diff, diff) + torch.trace(sigma1) + torch.trace(sigma2) - 2 * tr_covmean)
+    fid.requires_grad = True
     return fid
 
 
 def calculate_activation_statistics(img_tensor, model,
-                                    dims=2048, cuda=True):
-    act = get_activations(img_tensor, model)
+                                    dim=2048, cuda=True):
+    """Calculation of the statistics used by the FID.
+    Params:
+    -- img_tensor  : Pytorch tensor of images
+    -- model       : Instance of inception model
+    -- dims        : Dimensionality of features returned by Inception
+    
+    Returns:
+    -- mu    : The mean over samples of the activations of the pool_3 layer of
+               the inception model.
+    -- sigma : The covariance matrix of the activations of the pool_3 layer of
+               the inception model.
+    """                                
+    act = get_activations(img_tensor, model, dim)
     mu = act.mean(dim=0)
     sigma = torch.tensor(np.cov(act, rowvar=False))   
     return mu, sigma
 
 
-def fid(input, target, cuda=True, dim=2048):
-    """Calculates the FID of the outputs and the targets"""
+def fid(input, target, dim=2048):
+    """Calculates the FID of the inputs and the targets"""
     block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[dim]
-    model = InceptionV3([block_idx])
-    if cuda:
-        model.cuda()
+    model = InceptionV3([block_idx]).cuda()
+    model.eval()
 
     # m vector size dims 
     # s matrix size dims x dims
-    m1, s1 = calculate_activation_statistics(input, model, dim, cuda)
-    m2, s2 = calculate_activation_statistics(target, model, dim, cuda)
+    m1, s1 = calculate_activation_statistics(input, model, dim)
+    m2, s2 = calculate_activation_statistics(target, model, dim)
     
     fid_value = calculate_frechet_distance(m1, s1, m2, s2)
 
